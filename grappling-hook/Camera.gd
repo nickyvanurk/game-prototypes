e07febc6cdf04b4689b739c7315e735640ddab5f
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-25 16:53:03 +0100

    Add player movement

---
+++ grappling-hook/Camera.gd
@@ -0,0 +1,16 @@
+extends Camera
+
+onready var pivot = get_parent().find_node("CameraPivot")
+onready var mount = get_parent().find_node("CameraMount")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	set_as_toplevel(true)
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+func _process(delta):
+	transform.origin = lerp(global_transform.origin, mount.global_transform.origin, 0.5)
+	look_at(pivot.global_transform.origin, Vector3.UP)
--- grappling-hook/Player.gd
+++ grappling-hook/Player.gd
@@ -11,10 +11,11 @@ onready var gravity = ProjectSettings.get("physics/3d/default_gravity")
 onready var terminal_velocity = ProjectSettings.get("physics/3d/terminal_velocity")
 
 func _physics_process(delta):
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.z = Input.get_axis("move_forward", "move_backward")
+	direction = Vector3.ZERO
+	direction += transform.basis.x * Input.get_axis("move_left", "move_right")
+	direction += transform.basis.z * Input.get_axis("move_forward", "move_backward")
 	direction = direction.normalized()
-	
+
 	velocity.x = velocity.x + (direction.x * speed - velocity.x) * (acceleration * delta)
 	velocity.z = velocity.z + (direction.z * speed - velocity.z) * (acceleration * delta)
 	velocity.y = clamp(velocity.y - (gravity * delta), -terminal_velocity, terminal_velocity)
@@ -22,4 +23,4 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_impulse
 	
-	velocity = move_and_slide(velocity, Vector3.UP)
+	velocity = move_and_slide(velocity, Vector3.UP, true)

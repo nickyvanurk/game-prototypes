cc2d8e91ce3d2e8873c19d0603877acaac41edd1
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-01 22:51:35 +0100

    Refactor

--- procedural-animation/player/leg.gd
+++ procedural-animation/player/leg.gd
@@ -2,16 +2,17 @@ extends Position3D
 
 onready var player = get_node("/root/World/Player")
 
-var segments = []
+var bones = []
 
 func _ready():
-	for segment in get_children():
-		segments.push_back(segment)
+	for bone in get_children():
+		bones.push_back(bone)
 
 func _physics_process(delta):
-	for i in range(segments.size() - 1, -1, -1):
-		var segment = segments[i]
-		var target = player if i == segments.size() - 1 else segments[i + 1]
-		var distance = (target.global_transform.origin - segment.global_transform.origin).normalized() * segment.length
-		segment.look_at(target.global_transform.origin, Vector3.UP)
-		segment.global_transform.origin = target.global_transform.origin - distance
+	for i in range(bones.size() - 1, -1, -1):
+		var bone = bones[i]
+		var target = player if i == bones.size() - 1 else bones[i + 1]
+		var direction = bone.global_transform.origin.direction_to(target.global_transform.origin)
+		var distance = direction * bone.length
+		bone.look_at(target.global_transform.origin, Vector3.UP)
+		bone.global_transform.origin = target.global_transform.origin - distance

57b56da192a3358f192c18e61511a6bafe338b47
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-08 20:34:48 +0100

    Add model to player

---
+++ third-person-controller/player/player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody
+
+export(NodePath) var animation_tree
+
+var speed = 3
+var acceleration = 9
+var jump_impulse = 6.2
+
+var direction = Vector3.ZERO
+var velocity = Vector3.ZERO
+
+onready var gravity = ProjectSettings.get("physics/3d/default_gravity")
+onready var terminal_velocity = ProjectSettings.get("physics/3d/terminal_velocity")
+
+onready var anim_tree = get_node(animation_tree)
+
+func _physics_process(delta):
+	direction = Vector3.ZERO
+	direction += transform.basis.x * Input.get_axis("move_left", "move_right")
+	direction += transform.basis.z * Input.get_axis("move_forward", "move_backward")
+	direction = direction.normalized()
+
+	velocity.x = velocity.x + (direction.x * speed - velocity.x) * (acceleration * delta)
+	velocity.z = velocity.z + (direction.z * speed - velocity.z) * (acceleration * delta)
+	velocity.y = clamp(velocity.y - (gravity * delta), -terminal_velocity, terminal_velocity)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_impulse
+	
+	if direction != Vector3.ZERO:
+		anim_tree["parameters/playback"].travel("Run")
+	else:
+		anim_tree["parameters/playback"].travel("Idle")
+	
+	velocity = move_and_slide(velocity, Vector3.UP, true)

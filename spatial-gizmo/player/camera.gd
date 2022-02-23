fd99f3c505089b3da50105cbee04b4e4865e5d48
Nicky van Urk <nickyvanurk@gmx.com>
2022-02-23 15:03:24 +0100

    Rotate camera only on button press

--- spatial-gizmo/player/camera.gd
+++ spatial-gizmo/player/camera.gd
@@ -4,13 +4,8 @@ onready var pivot = get_parent().find_node("CameraPivot")
 onready var mount = get_parent().find_node("CameraMount")
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	set_as_toplevel(true)
 	
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		handle_mouse_capture()
-		
 func _process(delta):
 	transform.origin = lerp(global_transform.origin, mount.global_transform.origin, 0.5)
 	look_at(pivot.global_transform.origin, Vector3.UP)

4c396852dbbf5251312693feb9f9ead4da5082db
Nicky van Urk <nickyvanurk@gmx.com>
2022-02-24 20:45:00 +0100

    Add zoom

--- orbit-controls/player/camera_pivot.gd
+++ orbit-controls/player/camera_pivot.gd
@@ -4,7 +4,18 @@ export(float, 0.1, 1) var mouse_sensitivity  = 0.3
 export(float, -90, 0) var min_pitch = -90
 export(float, 0, 90) var max_pitch = 90
 
+export(float) var min_zoom = 1.0
+export(float) var max_zoom = 10.0
+export(float) var zoom_factor = 0.5
+export(float) var zoom_duration = 0.2
+
 onready var parent = get_parent()
+onready var camera_boom = $CameraBoom
+
+var zoom_level = 1.0 setget set_zoom_level
+
+func _ready():
+	zoom_level = camera_boom.spring_length
 
 func _input(event):
 	if Input.is_action_pressed("rotate_camera") and event is InputEventMouseMotion:
@@ -13,7 +24,11 @@ func _input(event):
 		rotation_degrees.x = clamp(rotation_degrees.x, min_pitch, max_pitch)
 	
 	if event.is_action_pressed("zoom_in"):
-		print("zoom in")
+		set_zoom_level(zoom_level - zoom_factor)
 		
 	if event.is_action_pressed("zoom_out"):
-		print("zoom out")
+		set_zoom_level(zoom_level + zoom_factor)
+
+func set_zoom_level(value):
+	zoom_level = clamp(value, min_zoom, max_zoom)
+	camera_boom.spring_length = zoom_level

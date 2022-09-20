19c78e9ca8919b0bdcbb61d25ac0c27a198e1b8e
Nicky van Urk <nickyvanurk@gmx.com>
2022-09-20 05:20:01 +0200

    Rework leg movement

--- procedural-animation/player/leg/leg.gd
+++ procedural-animation/player/leg/leg.gd
@@ -1,64 +1,63 @@
-@tool
 extends Node3D
 
 
 const CHAIN_MAX_ITER = 3
-const STEP_DISTANCE = 0.1
-const STEP_HEIGHT = 0.3
 
 @onready var bones = [$Bone1, $Bone2, $Bone3]
 @onready var target = $Target
 @onready var pole = $Pole
-@onready var ray = $Ray
+@onready var home_ray = $HomeRay
+@onready var overshoot_ray = $OvershootRay
 
+@export var step_distance = 0.1
+@export var step_duration = 0.2
+@export var step_overshoot_fraction = 1.25
+
+var is_moving = false
 var time = 0.0
-
-@onready var destination = $Target.global_transform.origin
-@onready var origin = $Target.global_transform.origin
+var start_point = Vector3()
+var center_point = Vector3()
+var end_point = Vector3()
 
 func _process(delta):
-	time += delta * 5
-
-	if time <= 1.0:
-		var mid_point = (origin + destination) / 2
-		mid_point.y += STEP_HEIGHT
-		target.global_transform.origin = origin.bezier_interpolate(mid_point, mid_point, destination, time)
-	elif origin != destination:
-		origin = destination
-	
 	_solve()
 
-func move_to_position(position: Vector3):
-	if origin == destination:
-		destination = position
-		origin = target.global_transform.origin
-		time = 0.0
+func step(direction, speed_frac, delta):
+	if !home_ray.is_colliding():
+		return
 	
-func is_moving():
-	return time < 1.0
+	var home_position = home_ray.get_collision_point()
+	var distance = target.global_position.distance_to(home_position)
+	if distance > step_distance:
+		if !is_moving:
+			is_moving = true
+			
+			# TODO: Rework later. Find good solution to match movement to speed
+			step_distance = speed_frac / 6
+			
+			start_point = target.global_position
+			var overshoot_distance = step_distance * step_overshoot_fraction
+			var overshoot_vector = direction * overshoot_distance
+			var overshoot_ray_position = home_position + overshoot_vector
+			overshoot_ray_position.y = overshoot_ray.global_position.y
+			overshoot_ray.global_position = overshoot_ray_position
+			end_point = overshoot_ray.get_collision_point()
+			center_point = (start_point + end_point) / 2
+			center_point += home_ray.get_collision_normal() * start_point.distance_to(end_point) / 2.0
+	
+	if is_moving:
+		time += delta
+		
+		var normalized_time = time / step_duration
+		# Quadratic bezier curve
+		target.global_position = (
+			start_point.lerp(center_point, normalized_time).lerp(
+			center_point.lerp(end_point, normalized_time), normalized_time
+		))
 
-func should_step():
-	if ray.is_colliding():
-		var distance = target.global_transform.origin.distance_to(ray.get_collision_point())
-		if distance >= STEP_DISTANCE:
-			return true
-	return false
-
-func step(direction, distance, time):
-	var dest = ray.get_collision_point() + direction.normalized() * distance
-	move_to_position(dest)
-#	destination = dest
-#	origin = Vector3(target.global_transform.origin)
-#	var tween = create_tween()
-#	tween.tween_method(tween_curve, 0.0, 1.0, time)
-
-#func tween_curve(t):
-#	var mid_point = (origin + destination) / 2
-#	mid_point.y += STEP_HEIGHT
-#	var mid_point = origin.slerp(destination, 0.5)
-#	mid_point.y += 0.1
-#	target.global_transform.origin = origin.bezier_interpolate(mid_point, mid_point, destination, t)
-#	target.global_transform.origin = origin.slerp(destination, t)
+		if time >= step_duration:
+			is_moving = false
+			time = 0.0
 
 func _solve():
 	if bones.size() > 2:
--- procedural-animation/player/player.gd
+++ procedural-animation/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export var speed = 0.3
+@export var speed = 0.6
 @export var acceleration = 9
 @export var jump_impulse = 6.2
 
@@ -20,18 +20,17 @@ func _physics_process(delta):
 	velocity.z = velocity.z + (direction.z * speed - velocity.z) * (acceleration * delta)
 	
 	move_and_slide()
-
-	if direction.length() > 0:
-		var q_from = global_transform.basis.get_rotation_quaternion()
-		var q_to = Transform3D().looking_at(direction).basis.get_rotation_quaternion()
-		global_transform.basis = Basis(q_from.slerp(q_to, 5 * delta))
 	
-	for idx in range(0, legs.size()):
-		var leg = legs[idx]
-		if !leg.is_moving() && leg.should_step() && !legs[(idx + 1) % 4].is_moving():
-			var leg_direction = velocity.normalized()
-			var leg_step_duration = 0.5
-			var leg_step_distance = 0.1
-			leg.step(leg_direction, leg_step_distance, leg_step_duration)
-			leg = legs[(idx + 2) % 4]
-			leg.step(leg_direction, leg_step_distance, leg_step_duration)
+#	if direction.length() > 0:
+#		var q_from = global_transform.basis.get_rotation_quaternion()
+#		var q_to = Transform3D().looking_at(direction).basis.get_rotation_quaternion()
+#		global_transform.basis = Basis(q_from.slerp(q_to, 5 * delta))
+	
+	var dir = velocity.normalized()
+	var speed_frac = velocity.length() / speed
+	if !legs[0].is_moving && !legs[2].is_moving:
+		legs[1].step(dir, speed_frac, delta)
+		legs[3].step(dir, speed_frac, delta)
+	if !legs[1].is_moving && !legs[3].is_moving:
+		legs[0].step(dir, speed_frac, delta)
+		legs[2].step(dir, speed_frac, delta)

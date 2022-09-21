08ff7f1d100f85eaa09421a424111c1220342399
Nicky van Urk <nickyvanurk@gmx.com>
2022-09-21 17:08:19 +0200

    Split ik and leg logic

--- procedural-animation/player/leg/leg.gd
+++ procedural-animation/player/leg/leg.gd
@@ -1,17 +1,15 @@
 extends Node3D
 
-
 const CHAIN_MAX_ITER = 3
-
-@onready var bones = [$Bone1, $Bone2, $Bone3]
-@onready var target = $Target
-@onready var pole = $Pole
-@onready var home_ray = $HomeRay
-@onready var overshoot_ray = $OvershootRay
 
 @export var step_distance = 0.1
 @export var step_duration = 0.2
 @export var overshoot_fraction = 1.25
+
+@onready var target = $Target
+@onready var ik = IK.new([$Bone1, $Bone2, $Bone3], target, $Pole)
+@onready var home_ray = $HomeRay
+@onready var overshoot_ray = $OvershootRay
 
 var is_moving = false
 var time = 0.0
@@ -20,7 +18,7 @@ var center_point = Vector3()
 var end_point = Vector3()
 
 func _process(delta):
-	_solve()
+	ik.solve()
 
 func step(direction, delta):
 	if !home_ray.is_colliding():
@@ -53,52 +51,3 @@ func step(direction, delta):
 		if time >= step_duration:
 			is_moving = false
 			time = 0.0
-
-func _solve():
-	if bones.size() > 2:
-		for idx in range(1, bones.size()):
-			var pole_pos = pole.global_transform.origin
-			var pole_pos_diff = pole_pos - bones[idx].global_transform.origin
-			bones[idx].global_transform.origin += pole_pos_diff
-
-	var base = bones[0].global_transform.origin
-
-	for i in range(CHAIN_MAX_ITER):
-		_iterate_backward(target)
-		_iterate_forward(base)
-		_apply_rotation()
-
-func _iterate_backward(target):
-	var size = bones.size()
-	if size < 2: return
-	
-	for i in range(size - 1, -1, -1):
-		var curr = target if i == size - 1 else bones[i + 1]
-		var prev = bones[i]
-		var direction = prev.global_transform.origin.direction_to(curr.global_transform.origin)
-		var offset = direction * prev.length
-		prev.global_transform.origin = curr.global_transform.origin - offset
-
-func _iterate_forward(base):
-	var size = bones.size()
-	if size < 2: return
-	
-	bones[0].global_transform.origin = base
-	
-	for i in range(1, size):
-		var curr = bones[i]
-		var prev = bones[i - 1]
-		var direction = prev.global_transform.origin.direction_to(curr.global_transform.origin)
-		var offset = direction * prev.length
-		curr.global_transform.origin = prev.global_transform.origin + offset
-
-func _apply_rotation():
-	var size = bones.size()
-	if size < 2: return
-	
-	for i in range(size - 1, -1, -1):
-		var curr = target if i == size - 1 else bones[i + 1]
-		var prev = bones[i]
-		var direction = prev.global_transform.origin.direction_to(curr.global_transform.origin)
-		var offset = direction * prev.length
-		prev.look_at(curr.global_transform.origin, Vector3.UP)

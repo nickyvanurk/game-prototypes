0b408b424f3eece06c76516625aceb5cede548f7
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-16 22:52:45 +0100

    Offset UV based on terrain mesh position

--- procedural-terrain/terrain.gd
+++ procedural-terrain/terrain.gd
@@ -9,3 +9,5 @@ func _snap(player):
 	var player_pos = player.global_transform.origin.snapped(Vector3(snap_step, 0, snap_step))
 	global_transform.origin.x = player_pos.x
 	global_transform.origin.z = player_pos.z
+	get_active_material(0).set("shader_param/uvx", player_pos.x / size)
+	get_active_material(0).set("shader_param/uvy", player_pos.y / size)

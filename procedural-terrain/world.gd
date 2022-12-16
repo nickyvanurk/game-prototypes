66de0781265b1db2235e2ae55252c797ac84b1bf
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-17 00:53:40 +0100

    Create timer in code instead

--- procedural-terrain/world.gd
+++ procedural-terrain/world.gd
@@ -1,11 +1,12 @@
 @tool
 extends Node3D
 
-@onready var timer = $Timer
 @onready var player = $Camera3D
 @onready var terrain = $MapGenerator/Terrain
 
 
 func _ready():
+	var timer = Timer.new()
+	add_child(timer)
 	timer.timeout.connect(Callable(terrain, "_snap").bind(player))
 	timer.start()

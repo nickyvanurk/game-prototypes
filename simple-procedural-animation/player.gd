f4bf665bb3ac2bef1902fa79434eeb441a690ab1
Nicky van Urk <nickyvanurk@gmx.com>
2023-01-05 13:14:35 +0100

    Remove gravity from player

--- simple-procedural-animation/player.gd
+++ simple-procedural-animation/player.gd
@@ -2,13 +2,8 @@ extends CharacterBody3D
 
 @export var speed = 1.0
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:

198deb2bd9cf7036ea35bbbca5ca2cd1095062d8
Nicky van Urk <nickyvanurk@gmx.com>
2023-05-09 14:57:51 +0200

    Generate height map

--- map-generator/map_generator.gd
+++ map-generator/map_generator.gd
@@ -3,18 +3,38 @@ extends Node
 
 @export var generate = false
 @export var size: int = 512
+@export var seed: int = 0
+@export var octaves: int = 6
+@export var frequency: float = 0.002
+@export var persistence: float = 0.5
+@export var lacunarity: float = 2.0
 
 @onready var preview = get_node("Preview")
 
 
 func _ready():
-	preview.texture = _generate_texture()
+	_generate()
 
 
 func _process(delta):
 	if generate:
-		preview.texture = _generate_texture()
+		_generate()
 		generate = false
+
+
+func _generate():
+	preview.texture = _generate_height_map()
+
+
+func _generate_height_map():
+	var n = FastNoiseLite.new()
+	n.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	n.seed = seed
+	n.frequency = frequency
+	n.fractal_octaves = octaves
+	n.fractal_gain = persistence
+	n.fractal_lacunarity = lacunarity
+	return ImageTexture.create_from_image(n.get_image(size, size))
 
 
 func _generate_texture():

ef88de55b8558763a468e81f737e66bdb9f6f49e
Nicky van Urk <nickyvanurk@gmx.com>
2023-05-11 23:50:44 +0200

    Generate terrain mesh on CPU instead (for now)

--- map-generator/map_generator.gd
+++ map-generator/map_generator.gd
@@ -4,7 +4,7 @@ extends Node
 
 @export var generate = false
 @export var world_size = 2048
-@export var chunk_size: int = 513
+@export var chunk_size: int = 512
 @export var chunk_offset = Vector2()
 @export var seed: int = 0
 @export var octaves: int = 6
@@ -48,9 +48,9 @@ func _generate():
 
 
 func _generate_colored_height_map(height_map) -> Image:
-	var colored_height_map = Image.create(chunk_size, chunk_size, false, Image.FORMAT_RGBA8)
-	for y in chunk_size:
-		for x in chunk_size:
+	var colored_height_map = Image.create(chunk_size + 1, chunk_size + 1, false, Image.FORMAT_RGBA8)
+	for y in chunk_size + 1:
+		for x in chunk_size + 1:
 			var value = height_map.get_pixel(x, y).r
 			
 			if value < deep_water:
@@ -79,13 +79,13 @@ func _generate_height_map() -> Image:
 	n.fractal_gain = persistence
 	n.fractal_lacunarity = lacunarity
 	
-	var half_chunk_size = chunk_size / 2
-	var height_map = Image.create(chunk_size, chunk_size, false, Image.FORMAT_RGBA8)
-	for y in chunk_size:
-		for x in chunk_size:
-			var value = n.get_noise_2d(x - half_chunk_size + (chunk_size * chunk_offset.x), y - half_chunk_size + (chunk_size * chunk_offset.y)) * 0.5 + 0.5
+	var half_chunk_size = (chunk_size + 1) / 2
+	var height_map = Image.create(chunk_size + 1, chunk_size + 1, false, Image.FORMAT_RGBA8)
+	for y in chunk_size + 1:
+		for x in chunk_size + 1:
+			var value = n.get_noise_2d(x - half_chunk_size + ((chunk_size + 1) * chunk_offset.x), y - half_chunk_size + ((chunk_size + 1) * chunk_offset.y)) * 0.5 + 0.5
 			
-			var falloff_value = maxf(absf(float(x + (chunk_size * chunk_offset.x)) / float(world_size) * 2 - (float(chunk_size) / float(world_size))), absf(float(y + (chunk_size * chunk_offset.y)) / float(world_size) * 2 - (float(chunk_size) / float(world_size))))
+			var falloff_value = maxf(absf(float(x + ((chunk_size + 1) * chunk_offset.x)) / float(world_size) * 2 - (float(chunk_size + 1) / float(world_size))), absf(float(y + ((chunk_size + 1) * chunk_offset.y)) / float(world_size) * 2 - (float(chunk_size + 1) / float(world_size))))
 			var a = 3.0 
 			var b = 2.2
 			falloff_value = pow(falloff_value, a) / (pow(falloff_value, a) + pow(b - b * falloff_value, a))
---
+++ map-generator/terrain_gen.gd
@@ -0,0 +1,51 @@
+@tool
+extends MeshInstance3D
+
+
+@export var generate = false
+@export var seed = 21
+@export var size = 128
+@export var height_scale = 100
+
+var map_generator = MapGenerator.new()
+
+
+func _ready():
+	map_generator.seed = seed
+	map_generator.chunk_size = size
+	var height_map = map_generator._generate_height_map()
+	_generate_terrain(height_map)
+
+
+func _process(delta):
+	if generate:
+		map_generator.seed = seed
+		map_generator.chunk_size = size
+		var height_map = map_generator._generate_height_map()
+		_generate_terrain(height_map)
+		generate = false
+
+
+func _generate_terrain(height_map: Image):
+	var half_size = size / 2
+	var surface_tool = SurfaceTool.new()
+	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	for z in size + 1:
+		for x in size + 1:
+			surface_tool.set_uv(Vector2(inverse_lerp(0, size, x), inverse_lerp(0, size, z)))
+			surface_tool.add_vertex(Vector3(-half_size + x, (height_map.get_pixel(x, z).r - 0.5) * height_scale, -half_size + z))
+	
+	var vert_idx = 0
+	for z in size:
+		for x in size:
+			surface_tool.add_index(vert_idx + 0)
+			surface_tool.add_index(vert_idx + 1)
+			surface_tool.add_index(vert_idx + size + 1)
+			surface_tool.add_index(vert_idx + size + 1)
+			surface_tool.add_index(vert_idx + 1)
+			surface_tool.add_index(vert_idx + size + 2)
+			vert_idx += 1
+		vert_idx += 1
+	
+	surface_tool.generate_normals()
+	mesh = surface_tool.commit()

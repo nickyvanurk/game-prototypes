b08f200a98c448303f1c325dca6e24f20f8cb9a9
Nicky van Urk <nickyvanurk@gmx.com>
2023-05-12 12:14:48 +0200

    Add terrain resolution param

--- map-generator/terrain_gen.gd
+++ map-generator/terrain_gen.gd
@@ -6,6 +6,7 @@ extends MeshInstance3D
 @export var seed = 21
 @export var size = 128
 @export var height_scale = 100
+@export var resolution = 1.0
 
 var map_generator = MapGenerator.new()
 
@@ -27,24 +28,25 @@ func _process(delta):
 
 
 func _generate_terrain(height_map: Image):
-	var half_size = size / 2
+	var scaled_size = size / resolution
+	var scaled_half_size = scaled_size / 2
 	var surface_tool = SurfaceTool.new()
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
-	for z in size + 1:
-		for x in size + 1:
-			surface_tool.set_uv(Vector2(inverse_lerp(0, size, x), inverse_lerp(0, size, z)))
-			var y = height_map.get_pixel(x, z).r
-			surface_tool.add_vertex(Vector3(-half_size + x, (y - 0.5) * height_scale, -half_size + z))
+	for z in scaled_size + 1:
+		for x in scaled_size + 1:
+			surface_tool.set_uv(Vector2(inverse_lerp(0, scaled_size, x), inverse_lerp(0, scaled_size, z)))
+			var y = height_map.get_pixel(x * resolution, z * resolution).r
+			surface_tool.add_vertex(Vector3(-scaled_half_size * resolution + x * resolution, (y - 0.5) * height_scale, -scaled_half_size * resolution + z * resolution))
 	
 	var vert_idx = 0
-	for z in size:
-		for x in size:
+	for z in scaled_size:
+		for x in scaled_size:
 			surface_tool.add_index(vert_idx + 0)
 			surface_tool.add_index(vert_idx + 1)
-			surface_tool.add_index(vert_idx + size + 1)
-			surface_tool.add_index(vert_idx + size + 1)
+			surface_tool.add_index(vert_idx + scaled_size + 1)
+			surface_tool.add_index(vert_idx + scaled_size + 1)
 			surface_tool.add_index(vert_idx + 1)
-			surface_tool.add_index(vert_idx + size + 2)
+			surface_tool.add_index(vert_idx + scaled_size + 2)
 			vert_idx += 1
 		vert_idx += 1
 	

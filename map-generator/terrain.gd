0470f981096751e1eebdd7222f244e46e97202b5
Nicky van Urk <nickyvanurk@gmx.com>
2023-05-12 21:38:01 +0200

    Refactor terrain chunk generation

--- map-generator/terrain.gd
+++ map-generator/terrain.gd
@@ -1,45 +1,64 @@
-extends Node3D
+@tool
+extends MeshInstance3D
 
 
-@export var chunk_size = 128
-@export var view_distance = 1000
-@export var viewer: Node3D
-@export var chunk_scene: PackedScene
-
-var map_generator = MapGenerator.new()
-var chunks = {}
-var total_chunks
+@export var generate = false
+@export var size = 128
+@export var height_scale = 50
 
 
 func _ready():
-	map_generator.seed = 21
-	map_generator.chunk_size = chunk_size
-	var height_map = map_generator._generate_height_map()
-	total_chunks = roundi(view_distance / chunk_size)
+	_generate()
 
 
 func _process(delta):
-	_update_chunks()
+	if generate:
+		_generate()
+		generate = false
 
 
-func _update_chunks():
-	var current_x = roundi(viewer.position.x / chunk_size)
-	var current_z = roundi(viewer.position.z / chunk_size)
+func _generate():
+	var n = FastNoiseLite.new()
+	n.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	n.seed = 21
+	n.frequency = 0.002
+	n.fractal_octaves = 6
+	n.fractal_gain = 0.5
+	n.fractal_lacunarity = 2.0
 	
-	for offset_z in range(-total_chunks, total_chunks):
-		for offset_x in range(-total_chunks, total_chunks):
-			var viewer_offset = Vector3(current_x - offset_x, 0, current_z - offset_z)
-			if chunks.has(viewer_offset):
-				if chunks[viewer_offset]._update_lod(viewer.position):
-					map_generator.chunk_offset.x = viewer_offset.x
-					map_generator.chunk_offset.y = viewer_offset.z
-					var height_map = map_generator._generate_height_map()
-					chunks[viewer_offset]._generate(height_map, viewer_offset, chunk_size)
-			else:
-				var chunk = chunk_scene.instantiate();
-				map_generator.chunk_offset.x = viewer_offset.x
-				map_generator.chunk_offset.y = viewer_offset.z
-				var height_map = map_generator._generate_height_map()
-				chunk._generate(height_map, viewer_offset, chunk_size)
-				chunks[viewer_offset] = chunk
-				add_child(chunk)
+	var st = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	var half_size = size / 2
+	var thisrow = 0
+	var prevrow = 0
+	var point = 0
+	
+	for z in size:
+		for x in size:
+			st.set_uv(Vector2(inverse_lerp(0, size, x), inverse_lerp(0, size, z)))
+			st.add_vertex(Vector3(-half_size + x, n.get_noise_2d(x, z) * height_scale, -half_size + z))
+			point += 1
+			
+			if z > 0 and x > 0:
+				st.add_index(prevrow + x - 1)
+				st.add_index(prevrow + x)
+				st.add_index(thisrow + x - 1)
+				st.add_index(prevrow + x)
+				st.add_index(thisrow + x)
+				st.add_index(thisrow + x - 1)
+
+		prevrow = thisrow
+		thisrow = point
+
+	st.generate_normals()
+	st.generate_tangents()
+	
+	mesh = st.commit()
+	
+	_update_shader()
+
+
+func _update_shader():
+	get_active_material(0).set_shader_parameter("min_height", -1.0 * height_scale)
+	get_active_material(0).set_shader_parameter("max_height", 1.0 * height_scale)
